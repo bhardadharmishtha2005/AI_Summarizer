ca615a60c34dbde934a757eef8b9492c83ce7de6
bhardadharmishtha2005 <bhardadharmishtha2005@gmail.com>
2026-05-10 16:44:06 +0530

    Fix CORS error to allow frontend connection

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # This allows all origins (ideal for your internship submission)
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
